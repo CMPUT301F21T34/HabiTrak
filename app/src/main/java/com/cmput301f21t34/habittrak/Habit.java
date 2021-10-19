02bf2c3581ecfee139b3edc7dc68217fd25324e1
Dakota Kryzanowski <dakotarj@outlook.com>
2021-10-18 21:55:33 -0600

    added Habit_Events tracking capabilities

--- app/src/main/java/com/cmput301f21t34/habittrak/Habit.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/Habit.java
@@ -2,6 +2,7 @@ package com.cmput301f21t34.habittrak;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
 
 /**
  * Habit
@@ -45,10 +46,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * getTitle
      *
-     * @author Dakota
-     *
      * getter function for Habit title
      *
+     * @author Dakota
      * @return String
      */
     public String getTitle(){
@@ -58,10 +58,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * setTitle
      *
-     * @author Dakota
-     *
      * setter function for Habit title
      *
+     * @author Dakota
      * @param title String to change Habits title to
      */
     public void setTitle(String title){
@@ -71,10 +70,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * getReason
      *
-     * @author Dakota
-     *
      * getter function for Habit reason
      *
+     * @author Dakota
      * @return String
      */
     public String getReason(){
@@ -84,10 +82,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * setReason
      *
-     * @author Dakota
-     *
      * setter function for Habit reason
      *
+     * @author Dakota
      * @param reason String to change Habits reason to
      */
     public void setReason(String reason){
@@ -97,10 +94,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * getStartDate
      *
-     * @author Dakota
-     *
      * getter function for Habit start date
      *
+     * @author Dakota
      * @return Calendar
      */
     public Calendar getStartDate(){
@@ -110,10 +106,9 @@ public class Habit implements Comparable<Habit> {
     /**
      * setStartDate
      *
-     * @author Dakota
-     *
      * setter function for Habit start date
      *
+     * @author Dakota
      * @param startDate Calendar to change Habits start date to
      */
     public void setStartDate(Calendar startDate){
@@ -121,23 +116,85 @@ public class Habit implements Comparable<Habit> {
     }
 
 
+    /**
+     * addHabitEvent
+     *
+     * adds a Habit_Event object to Habit's habit events
+     *
+     * @param habitEvent Habit_Event to be added
+     * @return boolean true if added
+     */
     public boolean addHabitEvent(Habit_Event habitEvent){
         return habitEvents.add(habitEvent);
     }
 
-    public Habit_Event getHabitEvent(int index){
-        return habitEvents.get(index);
-    }
 
-    public Habit_Event removeHabitEvent(int index){
-        return habitEvents.remove(index);
-    }
-
+    /**
+     * removeHabitEvent
+     *
+     * removes a specific Habit_Event object from Habit's habit events
+     *
+     * @author Dakota
+     * @param habit Habit_Event object to be removed
+     * @return boolean true if removed
+     */
     public boolean removeHabitEvent(Habit_Event habit){
         return habitEvents.remove(habit);
     }
 
+    /**
+     * removeHabitEvent
+     *
+     * removes a Habit_Event using its String id from Habit's habit events
+     *
+     * @author Dakota
+     * @param habitEventID String habit event id
+     * @return boolean true if a Habit_Event was removed
+     * @deprecated use removeHabitEvent(Habit_Event) instead
+     */
+    public boolean removeHabitEvent(String habitEventID){
 
+        boolean removed = false; // keeps track if a Habit_Event has been removed
+
+        // iterates through habitEvents
+        for (int index = 0; index < habitEvents.size(); index++){
+
+            // checks for matching habitEventID
+            if (habitEvents.get(index).getHabit_event_id() == habitEventID){
+
+                habitEvents.remove(index); // removes if found
+                removed = true;
+            }
+        }
+
+        return removed;
+    }
+
+    /**
+     * sortHabitEvents
+     *
+     * sorts a Habit's habit events Arraylist
+     *
+     * @author Dakota
+     */
+    public void sortHabitEvents(){
+
+        // Sorts with Habit_Event's compareTo method
+        habitEvents.sort(Habit_Event::compareTo);
+        // min API 24 needed
+
+    }
+
+
+    /**
+     * compareTo
+     *
+     * Uses Habit startDate for comparison in sorting
+     *
+     * @author Dakota
+     * @param habit Habit object to compare to
+     * @return int -1,0,1
+     */
     @Override
     public int compareTo(Habit habit) {
 

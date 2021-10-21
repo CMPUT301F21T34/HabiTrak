7e80e640de3f877347c1a8cf5259dcf7915ec4a5
Dakota Kryzanowski <dakotarj@outlook.com>
2021-10-21 16:56:05 -0600

    changed comments

--- app/src/main/java/com/cmput301f21t34/habittrak/LoginFragment.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/LoginFragment.java
@@ -91,8 +91,9 @@ public class LoginFragment extends Fragment {
         if (true) { // logic for allowing loggin in, for sake of testing is always true - Dakota
             passwordOk = true;
 
-            // we would populate mainUser after user validates
-            mainUser = new User("Dummy User");
+            // populate user after verification
+            mainUser = new User("dummyUser");
+
 
         }
 
@@ -106,7 +107,7 @@ public class LoginFragment extends Fragment {
      */
     public void startHomePage(View view){
         Intent intent = new Intent(getActivity(), BaseActivity.class);
-        intent.putExtra("mainUser", mainUser);
+        intent.putExtra("mainUser", mainUser); // passes mainUser through intent
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         getActivity().finish();

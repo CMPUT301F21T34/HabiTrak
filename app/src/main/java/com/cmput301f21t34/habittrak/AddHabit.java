20a35bf96e92760da123203a9e78212058fda969
Pranav Wadhwa <pwadhwa@ualberta.ca>
2021-10-22 17:29:55 -0600

    added days of week selector

--- app/src/main/java/com/cmput301f21t34/habittrak/AddHabit.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/AddHabit.java
@@ -2,9 +2,12 @@ package com.cmput301f21t34.habittrak;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -17,6 +20,7 @@ import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -36,6 +40,16 @@ public class AddHabit extends AppCompatActivity {
     TextView startDate;
     Calendar calendar;
     MaterialButton saveButton;
+    MaterialButton mondayButton;
+    MaterialButton tuesdayButton;
+    MaterialButton wednesdayButton;
+    MaterialButton thursdayButton;
+    MaterialButton fridayButton;
+    MaterialButton saturdayButton;
+    MaterialButton sundayButton;
+    boolean[] daysOfWeek = new boolean[]{true, true, true, true, true, true, true};;
+    int whiteColor = Color.WHITE;
+    int tealColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +65,18 @@ public class AddHabit extends AppCompatActivity {
         habitName = findViewById(R.id.habit_name_edit_text);
         habitReason = findViewById(R.id.habit_reason_edit_text);
         saveButton = findViewById(R.id.save_habit);
+        mondayButton = findViewById(R.id.monday_button);
+        tuesdayButton = findViewById(R.id.tuesday_button);
+        wednesdayButton = findViewById(R.id.wednesday_button);
+        thursdayButton = findViewById(R.id.thursday_button);
+        fridayButton = findViewById(R.id.friday_button);
+        saturdayButton = findViewById(R.id.saturday_button);
+        sundayButton = findViewById(R.id.sunday_button);
+
+        // getting color
+        // TODO: change the setting for dark mode as well
+        tealColor = ContextCompat.getColor(getBaseContext(), R.color.teal_200);
+
 
         // setting date
         calendar = Calendar.getInstance();
@@ -103,7 +129,74 @@ public class AddHabit extends AppCompatActivity {
         });
 
 
+        // button listeners
+        mondayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, mondayButton, 0);
+            }
+        });
 
+        tuesdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, tuesdayButton, 1);
+            }
+        });
+
+        wednesdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, wednesdayButton, 2);
+            }
+        });
+
+        thursdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, thursdayButton, 3);
+            }
+        });
+
+        fridayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, fridayButton, 4);
+            }
+        });
+
+        saturdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, saturdayButton, 5);
+            }
+        });
+
+        sundayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeButtonState(view, sundayButton, 6);
+            }
+        });
+    }
+
+    /**
+     * Change the color of the button and the arraylist for days of week
+     * @param view Button View
+     * @param button to change the color
+     * @param position which day to change
+     * @author Pranav
+     */
+
+    public void changeButtonState(View view, MaterialButton button, int position){
+        if(daysOfWeek[position]){
+            button.setBackgroundColor(whiteColor);
+            daysOfWeek[position] = false;
+        }
+        else{
+            button.setBackgroundColor(tealColor);
+            daysOfWeek[position] = true;
+        }
 
     }
 
@@ -137,14 +230,13 @@ public class AddHabit extends AppCompatActivity {
     }
 
     /**
-     * finish activity if all fiel
+     * finish activity if all fields are filled
      */
 
     public void finishActivityWithResult(){
         String name = habitName.getText().toString();
         String reason = habitReason.getText().toString();
-        boolean[] daysOn = new boolean[]{true, true, true, true, true, true, true};
-        Habit newHabit = new Habit(name, reason, calendar, daysOn);
+        Habit newHabit = new Habit(name, reason, calendar, daysOfWeek);
         //Bundle newHabitBundle = new Bundle();
         //newHabitBundle.putParcelable("newHabit", newHabit);
         Intent result = new Intent();

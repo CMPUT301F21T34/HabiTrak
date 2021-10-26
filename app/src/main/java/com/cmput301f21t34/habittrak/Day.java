58f294960236bb63253293a257f0f869ddfb54bf
Dakota Kryzanowski <dakotarj@outlook.com>
2021-10-25 21:15:45 -0600

    Created Day object for days of the week

---
+++ app/src/main/java/com/cmput301f21t34/habittrak/Day.java
@@ -0,0 +1,20 @@
+package com.cmput301f21t34.habittrak;
+
+/** Day
+ *
+ * enum Object for representing each day of the week
+ *
+ * @author Dakota
+ * @since 2021-10-25
+ * @version 1.0
+ *
+ */
+enum Day{
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN
+}

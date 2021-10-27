f89bb75422dd5f7e027cc8f221149235d1a7668c
Kaaden RumanCam <kaadenruman@hotmail.com>
2021-10-27 12:14:04 -0600

    progress for reference

--- app/src/main/java/com/cmput301f21t34/habittrak/SocialPageList.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/SocialPageList.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,11 +23,13 @@ public class SocialPageList extends ArrayAdapter<User> {
 
     private ArrayList<User> profiles;
     private Context context;
+    private User mainUser;
 
-    SocialPageList(Context context, ArrayList<User> profiles) {
+    SocialPageList(Context context, ArrayList<User> profiles, User mainUser) {
         super(context, 0, profiles);
         this.context = context;
         this.profiles = profiles;
+        this.mainUser = mainUser;
     }
 
     /**
@@ -45,7 +49,13 @@ public class SocialPageList extends ArrayAdapter<User> {
             view = LayoutInflater.from(context).inflate(R.layout.social_page_listview_content, parent, false);
         }
 
+        User profile = profiles.get(position);
 
+        TextView username = view.findViewById(R.id.username_social_page);
+        Button followButton = view.findViewById(R.id.follow_button_social_page);
+
+        username.setText(profile.getUsername());
+        followButton.setText(mainUser.getFollowingList().contains(profile) ? "Unfollow" : (mainUser.getFollowerList().contains(profile) ? "Follow Back" : "Follow"));
 
         return view;
     }

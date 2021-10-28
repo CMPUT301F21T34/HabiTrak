72d6d155a848aaa1c49a61e641da724ef67051ec
Pranav Wadhwa <pwadhwa@ualberta.ca>
2021-10-27 21:41:21 -0600

    created viewPager for tab layout

--- app/src/main/java/com/cmput301f21t34/habittrak/SocialActivity.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/SocialActivity.java
@@ -12,6 +12,10 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.cmput301f21t34.habittrak.socialFragments.FollowersFragment;
+import com.cmput301f21t34.habittrak.socialFragments.FollowingFragment;
+import com.cmput301f21t34.habittrak.socialFragments.RequestsFragment;
+import com.cmput301f21t34.habittrak.socialFragments.SearchFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -28,5 +32,74 @@ public class SocialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
         socialTab = findViewById(R.id.social_tab_layout);
+
+        FragmentManager fm = getSupportFragmentManager();
+        ViewStateAdapter viewStateAdapter = new ViewStateAdapter(fm, getLifecycle());
+        viewPager = findViewById(R.id.social_view_pager);
+        viewPager.setAdapter(viewStateAdapter);
+
+        socialTab.addTab(socialTab.newTab().setText("Followers"));
+        socialTab.addTab(socialTab.newTab().setText("Following"));
+        socialTab.addTab(socialTab.newTab().setText("Requests"));
+        socialTab.addTab(socialTab.newTab().setText("Search"));
+
+        socialTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                socialTab.selectTab(socialTab.getTabAt(position));
+            }
+        });
+
     }
+
+    private class ViewStateAdapter extends FragmentStateAdapter {
+
+        public ViewStateAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            // Hardcoded in this order, you'll want to use lists and make sure the titles match
+            if (position == 0) {
+                return new FollowersFragment();
+            }
+
+            if (position == 1) {
+                return new FollowingFragment();
+            }
+
+            if (position == 2) {
+                return new RequestsFragment();
+            }
+
+            return new SearchFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            // Hardcoded, use lists
+            return 4;
+        }
+    }
+
 }

b2c060a00d190a67544fef36edeef2d992b2cc29
Henry Vu <Vuducdung28@gmail.com>
2021-11-01 17:38:34 -0600

    deleted databaseDocument

--- app/src/main/java/com/cmput301f21t34/habittrak/databaseDocument.java
+++
@@ -1,9 +0,0 @@
-package com.cmput301f21t34.habittrak;
-
-import com.cmput301f21t34.habittrak.user.Database_Pointer;
-
-import java.util.ArrayList;
-
-public class databaseDocument {
-    public ArrayList<Database_Pointer> pointerList;
-}

a6be7a1c7c9709893c04d3b7fe2cb232292255c0
Dakota Kryzanowski <dakotarj@outlook.com>
2021-11-02 15:29:40 -0600

    fixed tests

--- app/src/test/java/com/cmput301f21t34/habittrak/HabitTest.java
+++ app/src/test/java/com/cmput301f21t34/habittrak/HabitTest.java
@@ -38,7 +38,8 @@ public class HabitTest {
         boolean[] onDays = {true, false, true, false, true, false, false};
         // use specific start date for future testing
         Calendar startDate = new GregorianCalendar(2021, 1, 1);
-        Habit mockHabit = new Habit(title, reason, startDate, onDays);
+        Habit mockHabit = new Habit(title, reason, startDate);
+        mockHabit.getOnDaysObj().setAll(onDays);
         return mockHabit;
     }
 
@@ -61,7 +62,7 @@ public class HabitTest {
             flag = true;
         // flag and isOnDay() should both return true if today is Monday, Wednesday or Friday
         // and both return false otherwise
-        assertEquals(flag,habit.isOnDay());
+        assertEquals(flag,habit.getOnDaysObj().isOnDay());
     }
 
     /**

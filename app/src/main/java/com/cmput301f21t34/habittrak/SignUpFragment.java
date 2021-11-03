1cfd18c003d49bb3b98911a4536685a4ea0c215a
nav-28 <pwadhwa@ualberta.ca>
2021-11-03 16:50:41 -0600

    added database functions #47

--- app/src/main/java/com/cmput301f21t34/habittrak/SignUpFragment.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/SignUpFragment.java
@@ -1,13 +1,19 @@
 package com.cmput301f21t34.habittrak;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cmput301f21t34.habittrak.user.Habit_List;
+import com.cmput301f21t34.habittrak.user.User;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -16,6 +22,15 @@ import com.google.android.material.textfield.TextInputLayout;
  */
 public class SignUpFragment extends Fragment {
 
+    TextInputLayout emailLayout;
+    TextInputEditText emailEditText;
+    TextInputLayout usernameLayout;
+    TextInputEditText usernameEditText;
+    TextInputLayout passwordLayout;
+    TextInputEditText passwordEditText;
+    MaterialButton signupButton;
+    DatabaseManager db = new DatabaseManager();
+    User currentUser = null;
 
 
     public SignUpFragment() {
@@ -33,13 +48,75 @@ public class SignUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.habi_signup_fragment, container, false);
 
-        TextInputLayout emailLayout = view.findViewById(R.id.signup_email_text_input);
-        TextInputEditText emailEditText = view.findViewById(R.id.signup_email_edit_text);
-        TextInputLayout usernameLayout = view.findViewById(R.id.signup_username_text_input);
-        TextInputEditText usernameEditText = view.findViewById(R.id.signup_username_edit_text);
-        TextInputLayout passwordLayout = view.findViewById(R.id.signup_password_text_input);
-        TextInputEditText passwordEditText = view.findViewById(R.id.signup_password_edit_text);
+        emailLayout = view.findViewById(R.id.signup_email_text_input);
+        emailEditText = view.findViewById(R.id.signup_email_edit_text);
+        usernameLayout = view.findViewById(R.id.signup_username_text_input);
+        usernameEditText = view.findViewById(R.id.signup_username_edit_text);
+        passwordLayout = view.findViewById(R.id.signup_password_text_input);
+        passwordEditText = view.findViewById(R.id.signup_password_edit_text);
+        signupButton = view.findViewById(R.id.signup_signup_button);
+
+
+        signupButton.setOnClickListener(view1 -> {
+            emailLayout.setError(null);
+            usernameLayout.setError(null);
+            passwordLayout.setError(null);
+
+            boolean fieldsFull = false;
+            // check fields empty
+            if(isEmpty(emailEditText)){
+                emailLayout.setError("Email Required");
+            }
+            else if(isEmpty(usernameEditText)){
+                usernameLayout.setError("Username Required");
+            }
+            else if(isEmpty(passwordEditText)){
+                passwordLayout.setError("Password Required");
+            }
+            else{
+                fieldsFull = true;
+            }
+            // check email
+            if(fieldsFull) {
+                if (!checkEmail(emailEditText.getText())) {
+                    emailLayout.setError("Email Already in Use");
+                }
+                else{
+                    Habit_List habit_list = new Habit_List();
+                    db.createNewUser(emailEditText.getText().toString(),
+                            usernameEditText.getText().toString(),
+                            passwordEditText.getText().toString(),
+                            "", habit_list);
+                    currentUser = db.getUser(emailEditText.getText().toString());
+                    startHomePage(view);
+                }
+            }
+
+
+            // if everything correct then start base activity
+        });
 
         return view;
+    }
+
+    public boolean checkEmail(@NonNull Editable userEmail){
+        String email = userEmail.toString();
+        boolean isValid = false;
+        if (db.isUnique(email)){
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    public boolean isEmpty(TextInputEditText text){
+        return text.getText().toString().length() == 0;
+    }
+
+    public void startHomePage(View view){
+        Intent intent = new Intent(getActivity(), BaseActivity.class);
+        intent.putExtra("user", currentUser);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        getActivity().finish();
     }
 }

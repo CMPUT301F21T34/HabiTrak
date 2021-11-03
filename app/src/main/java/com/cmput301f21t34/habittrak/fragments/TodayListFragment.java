6abffc0069e97c4c84043ffbcb107ea84583d112
nav-28 <pwadhwa@ualberta.ca>
2021-11-03 17:58:06 -0600

    fixed error

--- app/src/main/java/com/cmput301f21t34/habittrak/fragments/TodayListFragment.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/fragments/TodayListFragment.java
@@ -52,7 +52,7 @@ public class TodayListFragment extends Fragment {
 
     private ListView habitList;
     private ArrayAdapter<Habit> habitAdapter;
-    private Habit_List habitsData;
+    private Habit_List habitsData = new Habit_List();
 
     User mainUser;
     RecyclerView recyclerView;
@@ -64,7 +64,6 @@ public class TodayListFragment extends Fragment {
     public TodayListFragment(User mainUser) {
 
         this.mainUser = mainUser;
-        habitsData = mainUser.getHabitList();
     }
 
     @Override
@@ -82,15 +81,15 @@ public class TodayListFragment extends Fragment {
         // code to execute //
 
         // sample data (Does not show as we are getting habits from main User)
+        /*
         Calendar date = new GregorianCalendar(2021,1,31);
         Habit habit1 = new Habit("exercise dog", "some desc", date);
             habit1.getOnDaysObj().setTrue(Calendar.MONDAY);
             habit1.getOnDaysObj().setTrue(Calendar.FRIDAY);
         Habit habit2 = new Habit("go for a walk", "some desc 2", date);
             habit2.getOnDaysObj().setAll(new boolean[]{true, true, true, true, true, true, true});
-
-        Habit_List habitsData = new Habit_List();
-        habitsData.add(habit1); habitsData.add(habit2);
+        */
+        //habitsData.add(habit1); habitsData.add(habit2);
 
 
         // populates habit list
@@ -101,7 +100,6 @@ public class TodayListFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new TodayHabitRecyclerAdapter(habitsData);
         recyclerView.setAdapter(adapter);
-        Log.d("Habits Size", Integer.toString(habitsData.size()));
 
         // touch helper for dragging habits
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
@@ -148,6 +146,7 @@ public class TodayListFragment extends Fragment {
                     Collections.swap(habitsData, i, i - 1);
                 }
             }
+            Log.d("Habit", "moved");
             recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
             Log.d("Position", "From: " + Integer.toString(fromPosition) + " To: " + Integer.toString(toPosition));
             return true;
@@ -179,17 +178,17 @@ public class TodayListFragment extends Fragment {
 
         habitsData.clear(); // Make sure is clear
 
-        ArrayList<Habit> mainUserHabits = mainUser.getHabitList(); // get HabitsList
-
+        Habit_List mainUserHabits = mainUser.getHabitList(); // get HabitsList
+        Log.d("refreshHabit", Integer.toString(mainUserHabits.size()));
         // Iterates through all habits
-        for (int index = 0; index < mainUserHabits.size(); index++){
+        int index;
+        for (index = 0; index < mainUserHabits.size(); index++){
             if (mainUserHabits.get(index).getOnDaysObj().isOnDay() && mainUserHabits.get(index).isHabitStart()){ // If a habit is active today add
                 habitsData.add(mainUserHabits.get(index));
                 adapter.notifyDataSetChanged();
-                Log.d("Habits Size", Integer.toString(habitsData.size()));
             }
         }
-
+        Log.d("HabitSize", Integer.toString(index));
     }
 
 

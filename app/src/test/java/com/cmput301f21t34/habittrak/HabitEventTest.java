d0bbd2fb11fbd4ea3608e2712e969346e882ed5b
Dakota Kryzanowski <dakotarj@outlook.com>
2021-11-05 15:37:01 -0600

    fixed error

--- app/src/test/java/com/cmput301f21t34/habittrak/HabitEventTest.java
+++ app/src/test/java/com/cmput301f21t34/habittrak/HabitEventTest.java
@@ -1,14 +1,14 @@
 package com.cmput301f21t34.habittrak;
 
+import android.location.Location;
+
 import com.cmput301f21t34.habittrak.user.Habit;
 import com.cmput301f21t34.habittrak.user.HabitEvent;
-<<<<<<< HEAD
-=======
 
 import java.io.File;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
->>>>>>> 3992ef7e0b74bd9368073e44db4c74be7b0b8f58
+
 
 /**
  * Class for testing HabitEvent Objects
@@ -32,19 +32,15 @@ public class HabitEventTest {
      * @return Habit
      * Returns a new Habit object to perform tests on
      */
-<<<<<<< HEAD
-    /*
-    private Habit_Event mockHabitEvent() {
-=======
     private HabitEvent mockHabitEvent() {
->>>>>>> 3992ef7e0b74bd9368073e44db4c74be7b0b8f58
+
         String id = "173";
         String comment = "I read book 1";
         Habit habit = new Habit("Read Book");
         Location loc = new Location("");
         File photo = new File("");
         Calendar completeDate = new GregorianCalendar(2021, 6, 1);
-        HabitEvent mockHabitEvent = new HabitEvent(id, comment, completeDate, habit, loc, photo);
+        HabitEvent mockHabitEvent = new HabitEvent(comment, completeDate, loc, photo);
         return mockHabitEvent;
-    }*/
+    }
 }

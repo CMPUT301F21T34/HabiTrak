350cf2be055180d9e4e886043bb5e3f6e3648be6
Dakota Kryzanowski <dakotarj@outlook.com>
2021-11-10 11:46:46 -0700

    added log in method

--- app/src/main/java/com/cmput301f21t34/habittrak/auth/Auth.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/auth/Auth.java
@@ -1,7 +1,9 @@
 package com.cmput301f21t34.habittrak.auth;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -82,13 +84,103 @@ public class Auth {
                         }
                     }
                 });
-        
+
         if (authUser != null){
             return authUser.getEmail();
         }
         return null;
     }
 
+    public String logIn (String email, String password){
+
+        authUser = null;
+
+        // Make's sure a user isn't already logged in
+        String userEmail = getLoggedIn();
+        if (userEmail != null){
+            return userEmail;
+        }
+
+        // Authenticates
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            // User provided valid credentials
+
+                            authUser = mAuth.getCurrentUser();
+
+                            // Check if they are verified
+                            boolean verified = authUser.isEmailVerified();
+
+                            if (!verified) {
+                                // If not verified refuse them to log in
+                                authUser = null;
+
+                                // alert and give option to resend email
+                                boolean sendEmail = alertNotVerified();
+
+                                if (sendEmail) { authUser.sendEmailVerification(); }
+
+                            }
+
+                        } else {
+                            // User provided invalid credentials
+
+                            authUser = null;
+                            Toast.makeText(context, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                        
+                    }
+                });
+
+        if (authUser != null){
+            return authUser.getEmail();
+        }
+        return null;
+
+    }
+
+    /**
+     * Alerts the user that their account email is not verified and asks
+     * if they would like a new email to be sent
+     *
+     * @author Dakota
+     * @return boolean true if they would like a new email, false else wise
+     */
+    private boolean alertNotVerified(){
+
+        // final boolean[] to be set by inner class (builder)
+        final boolean[] sendEmail = new boolean[1];
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder
+                .setMessage("Would you like to resend a verification email?")
+                .setTitle("Email not Verified");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                // Send new email
+                sendEmail[0] = true;
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Don't send new email
+                sendEmail[0] = false;
+            }
+        });
+
+        return sendEmail[0];
+
+    }
 
 
 }

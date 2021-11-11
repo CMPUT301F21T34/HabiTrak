eba2bcefa47545ae34a708fc19c2dd5e699bb254
Aron Rajabi <aron70223@gmail.com>
2021-11-10 23:31:36 -0700

    copied allhabitsfragment into eventsfragment as a base to work from

--- app/src/main/java/com/cmput301f21t34/habittrak/fragments/EventsFragment.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/fragments/EventsFragment.java
@@ -1,7 +1,10 @@
 package com.cmput301f21t34.habittrak.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,9 +13,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import com.cmput301f21t34.habittrak.R;
+import com.cmput301f21t34.habittrak.ViewEditHabit;
 import com.cmput301f21t34.habittrak.recycler.HabitRecycler;
+import com.cmput301f21t34.habittrak.recycler.TodayHabitRecyclerAdapter;
 import com.cmput301f21t34.habittrak.user.Habit;
 import com.cmput301f21t34.habittrak.user.HabitList;
 import com.cmput301f21t34.habittrak.user.User;
@@ -32,18 +38,20 @@ import java.util.ArrayList;
 public class EventsFragment extends Fragment {
 
     // Attributes //
-
+    public static String TAG = "Habit_Events";
     // These are for the Recycler view
     private RecyclerView habitRecyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private HabitRecycler habitRecycler;
     private ArrayList<Habit> habitsDisplayList;
+    private TodayHabitRecyclerAdapter adapter;
 
-    User mainUser;
+    private User mainUser;
 
     public EventsFragment(User mainUser) {
         habitsDisplayList = new ArrayList<>();
         this.mainUser = mainUser;
+        this.adapter = new TodayHabitRecyclerAdapter(habitsDisplayList, false);
     }
 
 
@@ -51,17 +59,37 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.habi_events_fragment, container, false);
+        View view = inflater.inflate(R.layout.habi_all_habits_fragment, container, false);
 
         // Sets up views and manager for recycler view
-        habitRecyclerView = view.findViewById(R.id.events_recycler_view);
+        habitRecyclerView = view.findViewById(R.id.all_recycler_view);
         layoutManager = new LinearLayoutManager(getActivity());
 
 
-        // Sets up the recycler view with a list of all habits, and
-        // an array list for the recycler to use for display - Dakota
-        this.habitRecycler = new HabitRecycler(habitRecyclerView, layoutManager, habitsDisplayList, mainUser.getHabitList());
+        // set the click listener interface for the adapter
+        adapter.setHabitClickListener(new TodayHabitRecyclerAdapter.HabitClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Habit habit = habitsDisplayList.get(position);
+                Intent intent = new Intent(getContext(), ViewEditHabit.class);
+                intent.putExtra("HABIT", habit);
+                intent.putExtra("position", habit.getIndex());
+                viewHabitResultLauncher.launch(intent);
+            }
 
+            @Override
+            public void menuButtonOnClick(View view, int position) {
+                showMenu(view, position);
+            }
+
+            @Override
+            public void checkBoxOnClick(View view, int position) {
+                // empty function as no checkbox in this fragment
+            }
+        });
+        // creates a new habitRecycler class with the view and data
+        habitRecycler = new HabitRecycler(habitRecyclerView, layoutManager, habitsDisplayList, mainUser.getHabitList());
+        habitRecycler.setAdapter(adapter);
 
         return view;
     }
@@ -97,5 +125,33 @@ public class EventsFragment extends Fragment {
         // tells the adapter in recycler that the dataset has changed
         habitRecycler.notifyDataSetChanged();
 
+    }
+
+    // activity result launcher for view/edit activity
+    ActivityResultLauncher<Intent> viewHabitResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {}
+    );
+
+    /**
+     * showMenu
+     *
+     * create a menu when Image Button is clicked
+     * @param view view from the adapter to create the menu
+     * @param position position of habit from adapter
+     */
+    public void showMenu(View view, int position) {
+        PopupMenu menu = new PopupMenu(getContext(), view);
+        menu.getMenuInflater().inflate(R.menu.social_popup_menu, menu.getMenu());
+        menu.getMenu().add("Remove");
+        menu.show();
+
+        menu.setOnMenuItemClickListener(menuItem -> {
+            Habit habit = habitsDisplayList.get(position);
+            mainUser.removeHabit(habit);
+            refreshAllFragment();
+            Log.d(TAG,"Habit Removed");
+            return true;
+        });
     }
 }

b621ac110bb8b8ccd494598ee8b602f18fd17575
nav-28 <pwadhwa@ualberta.ca>
2021-11-15 16:31:26 -0700

    added layout

---
+++ app/src/main/java/com/cmput301f21t34/habittrak/SocialViewProfile.java
@@ -0,0 +1,14 @@
+package com.cmput301f21t34.habittrak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialViewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_view_profile);
+    }
+}

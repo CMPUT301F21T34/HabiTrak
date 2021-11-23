9696ae988e480b56d87f0d441f34aea7e9fca06d
Dakota Kryzanowski <dakotarj@outlook.com>
2021-11-23 16:04:07 -0700

    comment

--- app/src/main/java/com/cmput301f21t34/habittrak/TimeIgnoringComparator.java
+++ app/src/main/java/com/cmput301f21t34/habittrak/TimeIgnoringComparator.java
@@ -1,17 +1,27 @@
 package com.cmput301f21t34.habittrak;
 
+import com.cmput301f21t34.habittrak.user.Habit;
+
 import java.util.Calendar;
 import java.util.Comparator;
 
-// https://stackoverflow.com/questions/1439779/how-to-compare-two-dates-without-the-time-portion
-
+//
+/**
+ * TimeIgnoringComparator
+ *
+ * Compares two calendar dates, by the day not the hour or minute.
+ * @author Dakota
+ * @version 1.0
+ * @since 2021-11-23
+ * @see //https://stackoverflow.com/questions/1439779/how-to-compare-two-dates-without-the-time-portion
+ */
 
 public class TimeIgnoringComparator implements Comparator<Calendar> {
     public int compare(Calendar c1, Calendar c2) {
-        if (c1.get(Calendar.YEAR) != c2.get(Calendar.YEAR))
+        if (c1.get(Calendar.YEAR) != c2.get(Calendar.YEAR)) // Compares Years
             return c1.get(Calendar.YEAR) - c2.get(Calendar.YEAR);
-        if (c1.get(Calendar.MONTH) != c2.get(Calendar.MONTH))
+        if (c1.get(Calendar.MONTH) != c2.get(Calendar.MONTH)) // Compares Months
             return c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
-        return c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH);
+        return c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH); // Compares Days
     }
 }

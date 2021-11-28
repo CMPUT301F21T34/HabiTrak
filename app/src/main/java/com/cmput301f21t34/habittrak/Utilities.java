cae29aedf1240711c1e3fa6c8c28f8df773ba08a
Dakota Kryzanowski <dakotarj@outlook.com>
2021-11-27 23:14:57 -0700

    Created Utilities interface

---
+++ app/src/main/java/com/cmput301f21t34/habittrak/Utilities.java
@@ -0,0 +1,108 @@
+package com.cmput301f21t34.habittrak;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cmput301f21t34.habittrak.auth.LoginFragment;
+import com.cmput301f21t34.habittrak.streak.Streak;
+import com.cmput301f21t34.habittrak.user.Habit;
+import com.cmput301f21t34.habittrak.user.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+public interface Utilities {
+
+    DatabaseManager db = new DatabaseManager();
+
+    /**
+     * updates the database with a user's habit list
+     *
+     * @author Dakota
+     * @param user User to update in database
+     */
+    default void updateHabitListDB(User user){
+
+        db.updateHabitList(user.getEmail(), user.getHabitList());
+
+    }
+
+    /**
+     * refreshes all habit streaks
+     *
+     * @author Dakota
+     */
+    default void refreshHabitStreak(User user) {
+        // Refreshes all habit streaks //
+        ArrayList<Habit> habits = (ArrayList<Habit>) user.getHabitList(); // cast for simple iteration
+
+        for (int index = 0; index < habits.size(); index++){
+            Streak streak = new Streak(habits.get(index)); // set a Streak class to modify each habit
+            streak.refreshStreak(); // refreshes each streak
+        }
+    }
+
+    /**
+     * Go to login page
+     *
+     * @author Dakota
+     * @param activity Activity context to execute from (usually 'this')
+     */
+    default void goToLogin(AppCompatActivity activity) {
+        activity.getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.login_fragment_container, new LoginFragment(null))
+                .commit();
+    }
+
+
+
+    /**
+     * Go to Base Activity
+     *
+     * @author Dakota
+     * @param activity Activity context to execute from (usually 'this')
+     * @param user User to pass into base activity
+     */
+    default void goToBaseActivity(AppCompatActivity activity, User user) {
+        Intent intent = new Intent(activity, BaseActivity.class);
+
+        intent.putExtra("mainUser", user);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    /**
+     * Go to Main Activity
+     *
+     * @author Dakota
+     * @param activity Activity context to execute from (usually 'this')
+     */
+    default void goToMainActivity(AppCompatActivity activity){
+        // Send user back to main activity
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    /**
+     * Gets the main user from database.
+     *
+     * @author Dakota
+     * @param activity Activity context to execute from (usually 'this')
+     * @param user User object to populate
+     */
+    default void getMainUser(AppCompatActivity activity, User user) {
+        // Updates the mainUser, even if they are already logged in
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fUser == null){
+            goToMainActivity(activity);
+        }
+        user = db.getUser(fUser.getEmail());
+    }
+
+}

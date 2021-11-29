63eb6c1114d8629356b7ea4d5736900a48ce72ea
Tauseef Nafee Fattah <tauseefnafee@gmail.com>
2021-11-29 06:07:05 -0700

    Added the intent tests for habit events

--- app/src/androidTest/java/com/cmput301f21t34/habittrak/IntentTester.java
+++ app/src/androidTest/java/com/cmput301f21t34/habittrak/IntentTester.java
@@ -1,18 +1,20 @@
 package com.cmput301f21t34.habittrak;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -21,20 +23,67 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityResult;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.view.View;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.cmput301f21t34.habittrak.HabitActivity.AddHabitActivity;
+import com.cmput301f21t34.habittrak.HabitActivity.ViewEditHabitActivity;
+import com.cmput301f21t34.habittrak.event.AddHabitEventActivity;
+import com.cmput301f21t34.habittrak.event.MapsActivity;
+import com.cmput301f21t34.habittrak.event.ViewEditHabitEventActivity;
+import com.cmput301f21t34.habittrak.event.ViewHabitEvents;
 import com.cmput301f21t34.habittrak.user.Habit;
 
 import java.util.Calendar;
 
+/**
+ * A custom view action that handles the clicks inside an item of the recycler view
+ * Class was implemented from - "https://stackoverflow.com/questions/28476507/using-espresso-to-click-view-inside-recyclerview-item"
+ *
+ */
+class MyViewAction {
+
+    public static ViewAction clickChildViewWithId(final int id) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on a child view with specified id.";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                v.performClick();
+            }
+        };
+    }
+
+}
 public class IntentTester {
+
+
 
     // Rule to set, or else all intent checks will fail
     @Rule
@@ -51,6 +100,10 @@ public class IntentTester {
         Intents.release();
     }
 
+    /**
+     * Tests the signUp activity
+     * A new user is created using the signup page and then it is deleted at the end of the method
+     */
     @Test
     public void signUpTest(){
         //Click signup button
@@ -81,6 +134,10 @@ public class IntentTester {
         new DatabaseManager().deleteUser("iTestEmail@gmail.com");
     }
 
+    /**
+     * tests the addHabitActivity.
+     * At result a habit is added on the user's habit list. Also the habit frequency is tuesday,thursday
+     */
     @Test
     public void addHabitTest(){
         //Enter login information
@@ -119,6 +176,9 @@ public class IntentTester {
                 .perform(click());
     }
 
+    /**
+     * checks to see whether the habit has been added
+     */
     @Test
     public void habitAddedTest(){
         //Enter login information
@@ -153,6 +213,10 @@ public class IntentTester {
                 .perform(click());
     }
 
+    /**
+     * tests the profile Frag UI
+     * At result the bio of the user will be edited
+     */
     @Test
     public void profileFragTest(){
         //Enter login information
@@ -179,6 +243,10 @@ public class IntentTester {
         intended(hasComponent(MainActivity.class.getName()));
     }
 
+    /**
+     * tests the social Frags UI
+     * clicks on the tabs of the social section and see the whole lists in those tabs
+     */
     @Test
     public void socialFragUITest(){
         //Enter login information
@@ -210,6 +278,10 @@ public class IntentTester {
                 .perform(click());
     }
 
+    /**
+     *  tests the view habit test
+     *  clicks on the habit tab in navigation bar and sees the whole habit list
+     */
     @Test
     public void viewHabitTest(){
         //Enter login information
@@ -229,4 +301,184 @@ public class IntentTester {
         //Click on a habit
         SystemClock.sleep(2000);
     }
+
+    /**
+     * tests add habit event activity.
+     * At result the user creates the habit event of the habit in the 0th index
+     */
+    @Test
+    public void addHabitEventTest(){
+        //Enter login information
+        onView(withId(R.id.username_edit_text))
+                .perform(typeText("rajabi@ualberta.ca"), closeSoftKeyboard());
+        onView(withId(R.id.password_edit_text))
+                .perform(typeText("password"), closeSoftKeyboard());
+        //Click login to login
+        onView(withId(R.id.login_button))
+                .perform(click());
+        SystemClock.sleep(2000);
+        //Check to make sure we're on the intended activity, then click on the social button
+        intended(hasComponent(BaseActivity.class.getName()));
+
+        // click the item on the recycler view
+        onView(withId(R.id.today_recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.today_listview_checkbox)));
+        // check to make sure we are on the intended activity, then fill the new habit event form
+        intended(hasComponent(AddHabitEventActivity.class.getName()));
+        // fill the habit event form
+        onView(withId(R.id.Comment))
+                .perform(typeText("testing Habit Event"), closeSoftKeyboard());
+        onView((withId(R.id.mapButton))).perform(click());
+        // check to make sure that we're on the map activity
+        intended(hasComponent(MapsActivity.class.getName()));
+        // wait for the location to load
+        SystemClock.sleep(10000);
+        // ending the add map  activity
+        onView(withId(R.id.confirm_button)).perform(click());
+
+        // check to make sure that we're on the add habit event activity
+        intended(hasComponent(AddHabitEventActivity.class.getName()));
+        // take care of camera button
+
+        // take care of gallery button
+        Matcher<Intent> expectedIntent = allOf(hasAction(Intent.ACTION_PICK),hasData(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+        Resources resources = InstrumentationRegistry.getInstrumentation().getContext().getResources();
+        Uri imageUri = Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                        resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        );
+        Intent resultIntent = new Intent();
+
+        resultIntent.setData(imageUri);
+
+        ActivityResult activityResult = new ActivityResult(Activity.RESULT_OK,resultIntent);
+        intending(expectedIntent).respondWith(activityResult);
+        onView(withId(R.id.GalleryButton)).perform(click());
+        intended(expectedIntent);
+
+        // exit the add habit activity
+        onView(withId(R.id.addHabitEventButton)).perform(click());
+        intended(hasComponent(BaseActivity.class.getName()));
+        // click camera and gallery
+        //Logout to standardize the steps for the tests
+        onView(withId(R.id.navbar_menu_profile))
+                .perform(click());
+        onView(withId(R.id.logout))
+                .perform(click());
+
+    }
+
+    /**
+     * tests the viewEditHabitEvent activity.
+     * At result it edits the 1st event of the habit at position index 0 and then the user logs out
+     */
+    @Test
+    public void ViewEditHabitEventTest(){
+        //Enter login information
+        onView(withId(R.id.username_edit_text))
+                .perform(typeText("rajabi@ualberta.ca"), closeSoftKeyboard());
+        onView(withId(R.id.password_edit_text))
+                .perform(typeText("password"), closeSoftKeyboard());
+        //Click login to login
+        onView(withId(R.id.login_button))
+                .perform(click());
+        SystemClock.sleep(2000);
+        //Check to make sure we're on the intended activity,
+        intended(hasComponent(BaseActivity.class.getName()));
+        // go to the events fragment
+        onView(withId(R.id.navbar_menu_events)).perform(click());
+        //TODO:FIX the event recycler view id change it with the appropriate one
+        onView(withId(R.id.events_recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // check to see we're in the intended activity
+        intended(hasComponent(ViewHabitEvents.class.getName()));
+        onView(withId(R.id.events_recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        // check to see we're in the intended activity
+        intended(hasComponent(ViewEditHabitEventActivity.class.getName()));
+        SystemClock.sleep(2000);
+        onView(withId(R.id.comment_edit_text))
+                .perform(typeText("testing Edit Habit Event"), closeSoftKeyboard());
+        // take care of camera and gallery
+
+        // take care of gallery button
+        Matcher<Intent> expectedIntent = allOf(hasAction(Intent.ACTION_PICK),hasData(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+        Resources resources = InstrumentationRegistry.getInstrumentation().getContext().getResources();
+        Uri imageUri = Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                        resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                        resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        );
+        Intent resultIntent = new Intent();
+
+        resultIntent.setData(imageUri);
+
+        ActivityResult activityResult = new ActivityResult(Activity.RESULT_OK,resultIntent);
+        intending(expectedIntent).respondWith(activityResult);
+        onView(withId(R.id.gallery_button_edit)).perform(click());
+        intended(expectedIntent);
+
+
+        // exit the view edit habit event activity
+        onView(withId(R.id.save_habit_event_edit)).perform(click());
+        intended((hasComponent(ViewHabitEvents.class.getName())));
+        // get back to base activity
+        Espresso.pressBack();
+        intended(hasComponent(BaseActivity.class.getName()));
+        //Logout to standardize the steps for the tests
+        onView(withId(R.id.navbar_menu_profile))
+                .perform(click());
+        onView(withId(R.id.logout))
+                .perform(click());
+    }
+
+    /**
+     * tests the view edit habit Activity.
+     * At result the habit at index 0 (1st position) is edited and the user logs out.
+     */
+    @Test
+    public void ViewEditHabitActivityTest(){
+        //Enter login information
+        onView(withId(R.id.username_edit_text))
+                .perform(typeText("rajabi@ualberta.ca"), closeSoftKeyboard());
+        onView(withId(R.id.password_edit_text))
+                .perform(typeText("password"), closeSoftKeyboard());
+        //Click login to login
+        onView(withId(R.id.login_button))
+                .perform(click());
+        SystemClock.sleep(2000);
+        //Check to make sure we're on the intended activity,
+        intended(hasComponent(BaseActivity.class.getName()));
+        onView(withId(R.id.navbar_menu_habits)).perform(click());
+        onView(withId(R.id.all_recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        // check to make sure we're at the view edit habit activity
+        intended(hasComponent(ViewEditHabitActivity.class.getName()));
+        // edit the habit activity
+        onView(withId(R.id.view_habit_name_edit_text))
+                .perform(typeText("testing edit habit activity"), closeSoftKeyboard());
+        onView(withId(R.id.view_habit_reason_edit_text))
+                .perform(typeText("testing edit habit activity reason"), closeSoftKeyboard());
+        onView(withId(R.id.view_start_date_button))
+                .perform(click());
+        Espresso.pressBack();
+        onView(withId(R.id.view_save_habit))
+                .perform((click()));
+        intended(hasComponent(BaseActivity.class.getName()));
+        //Logout to standardize the steps for the tests
+        onView(withId(R.id.navbar_menu_profile))
+                .perform(click());
+        onView(withId(R.id.logout))
+                .perform(click());
+
+
+
+    }
+
 }
